7a5dc8f1d668fa14e6edf298b6d78e7dc4e8ca42
OrionMoran <OrionMoran@icloud.com>
2019-09-07 18:13:44 +0400

    first

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Кубик - парк</title>
+</head>
+<link rel="stylesheet" href="styles/main.css">
+<body>
+    <div class="leftMenu">
+        <div class="leftMenuTop">
+            <a href="../"><img class="logo1" src="img/logo1.png" alt=""></a>
+            <div class="mainMenuDiv">
+                <ul>
+                    <li>Перечень домов</li>
+                    <li>Инфраструктура</li>
+                    <li>Условия <br>приобретения</li>
+                    <li>Контакты</li>
+                </ul>
+            </div>
+        </div>
+        <div class="leftMenuBot">
+			<p class="leftMenuBotText1">Тольятти, с. Ягодное, <br>
+				улица Чудесная, дом 1Б</p>
+			<a class="leftMenuBotText2" href="tel: +7 (800) 756-09-05">+7 (800) 756-09-05</a>
+
+			<button class="btn1">Заказать звонок</button>
+			<ul class="socIcons">
+				<li><a href=""><img src="img/icon.svg" alt=""></a></li>
+				<li><a href=""><img src="img/icon-1.svg" alt=""></a></li>
+				<li><a href=""><img src="img/icon-2.svg" alt=""></a></li>
+				<li><a href=""><img src="img/icon-3.svg" alt=""></a></li>
+			</ul>
+			<p class="kkpk">
+			© 2018 «Кубик Парк»<br>
+			<a href="#">Политика конфиденциальности</a>
+
+			</p>
+        </div>
+	</div>
+	<div class="rightBlock">
+		<div class="mainheader">
+			<a href=""><img src="img/logo2.png" alt=""></a>
+			<div class="mainheaderRightDiv">
+				<a href="mailto:info@mechta-tlt.ru" class="headerEmail">info@mechta-tlt.ru</a>
+				<a class="mainheaderNumber" href="tel: +7 (800) 756-09-05">+7 (800) 756-09-05</a>
+				<button class="btn2">записаться на экскурсию</button>
+			</div>
+		</div>
+		<div class="rightBlockBot">
+			<div class="playVideo"><img src="img/play.svg" alt=""><p>Смотреть видео</p></div>
+		</div>
+	</div>
+</body>
+</html>
