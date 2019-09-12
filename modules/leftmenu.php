4675bc8afdad728206d9afee69ba14e580997b6b
OrionMoran <OrionMoran@icloud.com>
2019-09-13 02:36:40 +0400

    commit message

---
+++ modules/leftmenu.php
@@ -0,0 +1,31 @@
+<div class="leftMenu">
+        <div class="leftMenuTop">
+            <a href="../"><img class="logo1" src="img/logo1.png" alt=""></a>
+            <div class="mainMenuDiv">
+                <ul>
+                    <li><a href="houses.php">Перечень домов</a></li>
+                    <li>Инфраструктура</li>
+                    <li>Условия <br>приобретения</li>
+                    <li>Контакты</li>
+                </ul>
+            </div>
+        </div>
+        <div class="leftMenuBot">
+			<p class="leftMenuBotText1">Тольятти, с. Ягодное, <br>
+				улица Чудесная, дом 1Б</p>
+			<a class="leftMenuBotText2" href="tel: +7 (800) 756-09-05">+7 (800) 756-09-05</a>
+
+			<button class="btn1">Заказать звонок</button>
+			<ul class="socIcons">
+				<li><a href=""><img src="img/icon.svg" alt=""></a></li>
+				<li><a href=""><img src="img/icon-1.svg" alt=""></a></li>
+				<li><a href=""><img src="img/icon-2.svg" alt=""></a></li>
+				<li><a href=""><img src="img/icon-3.svg" alt=""></a></li>
+			</ul>
+			<p class="kkpk">
+			© 2018 «Кубик Парк»<br>
+			<a href="#">Политика конфиденциальности</a>
+
+			</p>
+        </div>
+	</div>
---
+++ modules/mainheader.php
@@ -0,0 +1,8 @@
+<div class="mainheader">
+    <a href=""><img src="img/logo2.png" alt=""></a>
+    <div class="mainheaderRightDiv">
+        <a href="mailto:info@mechta-tlt.ru" class="headerEmail">info@mechta-tlt.ru</a>
+        <a class="mainheaderNumber" href="tel: +7 (800) 756-09-05">+7 (800) 756-09-05</a>
+        <button class="btn2">записаться на экскурсию</button>
+    </div>
+</div>
